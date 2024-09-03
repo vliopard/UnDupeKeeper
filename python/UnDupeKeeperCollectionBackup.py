870b0b42c6cbd94e7a09fc1258027560e20b28eb
Vincent Liopard. <vliopard@biucsproject.org>
2024-09-03 02:41:00 -0300

    Size control of listing

--- python/UnDupeKeeperCollectionBackup.py
+++ python/UnDupeKeeperCollectionBackup.py
@@ -15,18 +15,21 @@ def duplicate_collection(collection_name):
     new_collection.insert_many(mongo_collection.find())
 
 
-def list_collections():
+def list_collections(size):
     collection_list = mongo_database.list_collection_names()
     print('LIST OF COLLECTIONS:')
     print(f'{section_line(constants.SYMBOL_UNDERLINE, constants.LINE_LEN)}')
     for item in sorted(collection_list):
         collection = mongo_database[item]
-        stats = mongo_database.command("collstats", item)
-        size_bytes = stats['size']
-        size_bytes_str = f'{size_bytes:,}'
-        count = collection.count_documents({})
-        count_str = f'{count:,}'
-        print(f'{constants.DATABASE_NAME}:[ {item.rjust(30)} ] [{count_str.rjust(9)}] [{size_bytes_str.rjust(13)}]')
+        if size:
+            stats = mongo_database.command("collstats", item)
+            size_bytes = stats['size']
+            size_bytes_str = f'{size_bytes:,}'
+            count = collection.count_documents({})
+            count_str = f'{count:,}'
+            print(f'{constants.DATABASE_NAME}:[ {item.rjust(30)} ] [{count_str.rjust(9)}] [{size_bytes_str.rjust(13)}]')
+        else:
+            print(f'{constants.DATABASE_NAME}:[ {item.rjust(30)} ]')
     print(f'{section_line(constants.SYMBOL_OVERLINE, constants.LINE_LEN)}')
 
 
@@ -39,10 +42,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Create a collection backup')
     parser.add_argument('-n', '--collection_name', type=str, help='New backup name')
     parser.add_argument('-l', '--collection_list', action='store_true', help='List collections')
+    parser.add_argument('-s', '--collection_size', action='store_true', help='List collections')
     parser.add_argument('-d', '--collection_delete', type=str, help='Delete collection')
     args = parser.parse_args()
     if args.collection_list:
-        list_collections()
+        list_collections(args.collection_size)
     elif args.collection_name:
         duplicate_collection(args.collection_name)
     elif args.collection_delete:
